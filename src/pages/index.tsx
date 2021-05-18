728a9bcbde449f225554733b39f78fa2a6125d86
Marco Ferreira <marcoavfcc@gmail.com>
2021-05-18 18:26:54 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
+
 import styles from './home.module.scss';
+import logo from '../assets/Vector.svg';
 
 interface Post {
   uid?: string;
@@ -26,9 +28,28 @@ interface HomeProps {
 
  export default function Home({ postsPagination }: HomeProps) {
    // TODO
+
    return(
-     <div>
-       <h1>Ola mundo!</h1>
+     <div className={styles.container}>
+
+       <div className={styles.header}>
+         <img src={logo} alt="Logo do blog" />
+         <span>spacetraveling</span>
+       </div>
+
+       <div className={styles.preview}>
+         <h1>Como ultilizar hooks</h1>
+         <p>Pensando em sicronização em fez de ciclos de vida.</p>
+         <div>
+            <time>15 Mar 2021</time>
+            <span>icone</span>
+            <p>Joseph Oliveira</p>
+         </div>
+
+
+       </div>
+
+
      </div>
    )
  }
